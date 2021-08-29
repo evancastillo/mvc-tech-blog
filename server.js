5727a29696b62ad3791511b461105157863ea3d9
evancastillo <evancastillo1992@gmail.com>
2021-08-29 16:08:17 -0700

    heroku

--- server.js
+++ server.js
@@ -35,7 +35,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+  app.listen(PORT, () => console.log('Now listening'));
 });
