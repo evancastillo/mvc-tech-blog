a6ad5f5eb67637bc95c2dbf0978c175fdb202d44
evancastillo <evancastillo1992@gmail.com>
2021-08-29 16:23:14 -0700

    heroku

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
+app.set('port', PORT);
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
